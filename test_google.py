d28f243662d6a265f3150a6b3809b06f1da8be22
rohit21-08 <rohitjha@live.in>
2026-04-05 23:41:19 +0530

    fix selenium issue

--- test_google.py
+++ test_google.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 def test_google():
     print("Auto trigger test")
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+
     driver.get("https://www.google.com")
     assert "Google" in driver.title
+
     driver.quit()
